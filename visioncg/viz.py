2c24e52184095c468c955465461b48507efc9172
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2019-01-16 16:53:09 +0100

    Implementation of plot_image_channels

--- visioncg/viz.py
+++ visioncg/viz.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 def draw_line(canvas_im, line, color=[255, 0, 0], thickness=2):
@@ -12,6 +13,7 @@ def draw_lines_on_image(canvas_im, lines, color=[255, 0, 0], thickness=2):
 
     for i in range(lines.shape[0]):
         draw_line(canvas_im, lines[i, :], color, thickness)
+
 
 def plot_line(line, **kvargs):
 
@@ -32,3 +34,27 @@ def plot_homogeneous_line_vector(vec, x_from, x_to, **kvargs):
     ys = line_func(xs)
 
     plt.plot(xs, ys, **kvargs)
+
+
+def plot_image_channels(im, figsize=None, titles=None): 
+
+    n_channels = im.shape[-1]
+    
+    show_titles = False
+    if titles is not None:
+        assert n_channels == len(titles)
+
+    if figsize is None:
+        plt.figure()
+    else:
+        plt.figure(figsize=figsize)
+
+    for i in range(n_channels):
+        
+        plt.subplot(1, n_channels, i + 1)
+
+        channel = im[:, :, i]
+        plt.imshow(channel)
+        plt.title(titles[i])
+
+
