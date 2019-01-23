2ae7cb1ad9b1b0aa3eabd97377da7d3c19b8f1fe
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2019-01-23 09:06:07 +0100

    Refactoring in features

--- visioncg/features.py
+++ visioncg/features.py
@@ -15,6 +15,41 @@ METHOD_INIT = {
 }
 
 
+def match_descriptors(descr_1, descr_2, normType, crossCheck):
+    """
+    Match keypoint descriptors using cv2.BFMatcher
+    and return the matches sorted by distance.
+    """
+
+    matcher = cv2.BFMatcher(normType, crossCheck)
+    matches = matcher.match(descr_1, descr_2)
+
+    return sorted(matches, key=(lambda m : m.distance))
+
+
+def gather_keypoints(keypoints_1, keypoints_2, matches):
+    """
+    Gather matched keypoints in a (n x 4) array,
+    where each row correspond to a pair of matching 
+    keypoints' coordinates in two images.
+    """
+
+    res = []
+
+    for m in matches:
+
+        idx_1 = m.queryIdx
+        idx_2 = m.trainIdx
+
+        pt_1 = keypoints_1[idx_1].pt
+        pt_2 = keypoints_2[idx_2].pt
+
+        row = [pt_1[0], pt_1[1], pt_2[0], pt_2[1]]
+        res.append(row)
+
+    return np.array(res)
+
+
 def create_kwargs_dict_if_not_none(param_names, param_vals):
     return {p_name: p_val for p_name, p_val in zip(param_names, param_vals) if p_val != None}
 
@@ -55,14 +90,7 @@ def create_feature_matching_cg(method):
 
     param_names = METHOD_PARAMS[method]
 
-    def match_func(descr_1, descr_2, normType, crossCheck):
-
-        matcher = cv2.BFMatcher(normType, crossCheck)
-        matches = matcher.match(descr_1, descr_2)
-
-        return sorted(matches, key=(lambda m : m.distance))
-
-    add_func_dict = {'match': match_func}
+    add_func_dict = {'match': match_descriptors}
     add_func_io = {
         'match': (('descriptors_1', 'descriptors_2', 'normType', 'crossCheck'), 'matches')
     }
@@ -74,23 +102,6 @@ def create_feature_matching_cg(method):
 
 
 def create_extended_feature_matching_cg(method):
-
-    def gather_keypoints(keypoints_1, keypoints_2, matches):
-
-        res = []
-
-        for m in matches:
-
-            idx_1 = m.queryIdx
-            idx_2 = m.trainIdx
-
-            pt_1 = keypoints_1[idx_1].pt
-            pt_2 = keypoints_2[idx_2].pt
-
-            row = [pt_1[0], pt_1[1], pt_2[0], pt_2[1]]
-            res.append(row)
-
-        return np.array(res)
 
     cg = create_feature_matching_cg(method)
 
