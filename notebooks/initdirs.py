5bd15206711ef36d97ce726a009d6accd8612fca
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2020-01-23 14:45:55 +0100

    Remove all notebooks and sample images
    Now part of the "VisionCG-Notebooks" repository

--- notebooks/initdirs.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-CODE_DIR = os.environ['PHD_CODE']
-ROOT_DIR = os.path.abspath('..')
-DATA_DIR = os.path.join(ROOT_DIR, 'data')
-
-sys.path.append(os.path.join(CODE_DIR, 'EPypes'))
-sys.path.append(ROOT_DIR)
