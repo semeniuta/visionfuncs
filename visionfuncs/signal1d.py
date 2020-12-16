1bb92b704a9decb8c5c9822194f2cf8ab97818ca
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2020-12-16 16:40:04 +0100

    Implement downsample_by_interpolate

--- visionfuncs/signal1d.py
+++ visionfuncs/signal1d.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import signal
+from scipy import interpolate
 
 
 def find_good_peaks(s, base_diff_threshold=20):
@@ -17,3 +18,20 @@ def find_good_peaks(s, base_diff_threshold=20):
     base_diff_abs = np.abs(bases_left - bases_right)
     
     return peaks[base_diff_abs > base_diff_threshold]
+
+
+def downsample_by_interpolate(s, n):
+    """
+    Dowsample a 1D signal s to a signal 
+    of size n by performing linear
+    interpolation of the original signal. 
+    """
+
+    sig_len = len(s)
+    domain = np.arange(sig_len)
+    f = interpolate.interp1d(domain, s)
+
+    step = sig_len / n
+    x = np.arange(0, sig_len, step)
+    
+    return f(x)
